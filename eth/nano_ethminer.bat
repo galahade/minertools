11785b289824dcaa2917ec661dea775c1c3f659b
Young Yang <galahade@sina.com>
2017-08-18 16:36:35 +0800

    add windows bat file

---
+++ eth/nano_ethminer.bat
@@ -0,0 +1,7 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+
+ethminer.exe --farm-recheck 200 -U -S eth-asia1.nanopool.org:9999 -FS eth-us-west1.nanopool.org:9999 -O 0xC71c4d0675Bf6D12C8A08d449d4966562eb3e88A.p106-2/galahade@sina.com
